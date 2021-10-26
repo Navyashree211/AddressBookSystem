3663303b9d5ce7e649870cd6af69b4758c323eec
navyas211@gmail.com <navyas211@gmail.com>
2021-10-26 19:15:07 +0530

    [NAVYA]ADDED: To add multiple person to address book

--- src/com/addressbooksystem/ContactPerson.java
+++ src/com/addressbooksystem/ContactPerson.java
@@ -1,8 +1,8 @@
 package com.addressbooksystem;
 
-public class ContactPerson {   
-	private String firstName,lastName, city,state, email ;
-	private long phoneNumber,zip;
+public class ContactPerson {    
+	private String firstName, lastName, city, state, email;
+	private long zip,phoneNumber;
 	
 	ContactPerson(String firstName, String lastName, String email, String city, String state,long phoneNumber, long zip)  {
 		super();
@@ -15,19 +15,19 @@ public class ContactPerson {
 		this.email = email;
 	}
 
-	public String getFirstName() {
+	public String getfirstName() {
 		return firstName;
 	}
 
-	public void setFirstName(String firstName) {
+	public void setfirstname(String firstName) {
 		this.firstName = firstName;
 	}
 
-	public String getLastName() {
+	public String getlastname() {
 		return lastName;
 	}
 
-	public void setLastName(String lastName) {
+	public void setlastName(String lastName) {
 		this.lastName = lastName;
 	}
 
@@ -55,28 +55,28 @@ public class ContactPerson {
 		this.email = email;
 	}
 
-	public long getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(long phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
 	public long getZip() {
 		return zip;
 	}
 
-	public void setZip(long zip) {
+	public void setZip(int zip) {
 		this.zip = zip;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhonenumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
 	}
 
 	@Override
 	public String toString() {
-		return "ContactPerson [firstName=" + firstName + ", lastName=" + lastName + ", city=" + city + ", state="
-				+ state + ", email=" + email + ", phoneNumber=" + phoneNumber + ", zip=" + zip + "]";
-	}
-	
+		return "Contact details:" + "\n" + "FirstName=" + firstName + "\n" + "LastName=" + lastName + "\n" + "City="
+				+ city + "\n" + "State=" + state + "\n" + "Zip=" + zip + "\n" + 
+				 "PhoneNumber=" + phoneNumber + "\n" + "Email=" + email;
 
+	}
 
 }
